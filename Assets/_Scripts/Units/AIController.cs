689f28e9df483a8a16f0a494a14f588aaae8d661
Jp Mendoza <jeanpauljr.mendoza@gmail.com>
2023-09-15 22:34:33 +0800

    Begun making AI
    Made the scripts

---
+++ Assets/_Scripts/Units/AIController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIController : Controller
+{
+    // AI CONTROLLER CAN BE ENEMY OR WHATEVER
+    // SHOULD HAVE THINGS BOTH ENEMY AND ALLY AI HAS
+}
